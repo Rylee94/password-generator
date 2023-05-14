a9ff976820e53a538fb2340467ef420413795e25
Rylee94 <128653866+Rylee94@users.noreply.github.com>
2023-05-14 00:06:05 -0500

    added function that prompts user for password criteria and a new variable called allChar that will hold possible characters that could be included in password

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 
+// these are my global variables
 var generateBtn = document.querySelector("#generate");
+//this is how many characters the resulting password will be
 var passwordLength;
 //wether to include lowercase characters in password
 var includeLowerCase;
@@ -11,7 +13,6 @@ var includeNumeric;
 //wether or not to include special characters in password
 var includeSpecialChar;
 //an array of characters that are numbers
-
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 //an array of special characters
 var specialChar = [
@@ -104,12 +105,44 @@ var upperCase = [
   "Y",
   "Z",
 ];
-
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
+//this function will ask the user for password criteria.
+function writePassword() {
+  //this will hold all of the possible characters that could be included in the password
+  var allChar = [];
+  passwordLength = window.prompt(
+    "Enter desired length of password between 8-128"
+  );
+  includeUpperCase = window.confirm("Do you want to include upercase letters?");
+  includeLowerCase = window.confirm(
+    "Do you want to include lowercase letters?"
+  ); //window.prompt("Do you want to include lowercase letters?");
+  includeNumeric = window.confirm("Do you want to include numbers?");
+  includeSpecialChar = window.confirm(
+    "Do you want to include special characters?"
+  );
+
+  //If include lower case is true, allChar now = lowercase.
+  if (includeLowerCase === true) {
+    allChar = allChar.concat(lowerCase);
+  }
+  //If include upper case is true (the user hit okay), we add uppercase to allChar.
+  if (includeUpperCase === true) {
+    allChar = allChar.concat(upperCase);
+  }
+  //If include numeric is true, we add numeric to allChar
+  if (includeNumeric === true) {
+    allChar = allChar.concat(numeric);
+  }
+  //If include specialChar is true, we add special characters to allChar
+  if (includeSpecialChar === true) {
+    allChar = allChar.concat(specialChar);
+  }
+
+  //use generatePassword to create a password
+  var password = generatePassword(allChar);
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
